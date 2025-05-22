5104aafd2555dc395268f87c514f5f74259c34ad
Tuan Nguyen <anhtuank7c@hotmail.com>
2025-05-22 09:58:26 +0700

    Update pipe_example.py

--- docs/multiprocessing/example/pipe_example.py
+++ docs/multiprocessing/example/pipe_example.py
@@ -1,2 +1,17 @@
 from multiprocessing import Pipe, Process
 from multiprocessing.connection import PipeConnection
+
+
+def pipe_worker(conn: PipeConnection):
+    conn.send("Data from pipe worker")
+    conn.close()
+
+
+if __name__ == "__main__":
+    print("Start")
+    parent_conn, child_conn = Pipe()
+    pipe_process = Process(target=pipe_worker, args=(child_conn,))
+    pipe_process.start()
+    pipe_process.join()
+    print(parent_conn.recv())
+    print("Done")
